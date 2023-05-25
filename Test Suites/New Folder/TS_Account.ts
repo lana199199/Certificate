71a2786d0945560e94b2b826efc53c1b30c4e8c5
thanh.pnguyen <thanh.pnguyen@10.10.33.152>
2023-05-25 13:14:27 +0700

    25 May

--- Test Suites/New Folder/TS_Account.ts
+++ Test Suites/New Folder/TS_Account.ts
@@ -8,14 +8,21 @@
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>124906c5-47e3-4563-868a-6d8da3f31d1a</testSuiteGuid>
    <testCaseLink>
       <guid>81aff9d3-5731-4dc0-ba8d-3a3655d9b638</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Account/Login fails</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f89ab6c6-4d02-4395-aefa-cad33b6ba812</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Account/Log out</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>

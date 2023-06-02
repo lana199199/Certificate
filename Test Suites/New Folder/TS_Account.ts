a8993535294e686aa4e4f52bb482fa4ce73ce7bb
thanh.pnguyen <thanh.pnguyen@10.10.32.34>
2023-06-02 10:01:14 +0700

    2Jun 10h

--- Test Suites/New Folder/TS_Account.ts
+++ Test Suites/New Folder/TS_Account.ts
@@ -5,23 +5,23 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>6</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>124906c5-47e3-4563-868a-6d8da3f31d1a</testSuiteGuid>
    <testCaseLink>
       <guid>81aff9d3-5731-4dc0-ba8d-3a3655d9b638</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Account/Login fails</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>f89ab6c6-4d02-4395-aefa-cad33b6ba812</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Account/Log out</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
